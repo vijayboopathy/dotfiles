846c5275c7c790d371f7c5b78f8715fab74df0a2
vijayboopathy <vijayboopathy.e@gmail.com>
2025-07-22 15:45:15 +0530

    Add more tmux config

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,14 +1,12 @@
 return { -- You can easily change to a different colorscheme.
-	"folke/tokyonight.nvim",
+	"catppuccin/nvim",
 	priority = 1000, -- Make sure to load this before all the other start plugins.
 	config = function()
 		---@diagnostic disable-next-line: missing-fields
-		require("tokyonight").setup({
-			styles = {
-				comments = { italic = false }, -- Disable italics in comments
-			},
+		require("catppuccin").setup({
+			flavor = "frappe",
 		})
-		vim.cmd.colorscheme("tokyonight-night")
+		vim.cmd.colorscheme("catppuccin")
 		vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 	end,
 }
