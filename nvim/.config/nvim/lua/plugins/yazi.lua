81a2941b48f028273d23c26f2f8b90520713ff06
vijayboopathy <vijayboopathy.e@gmail.com>
2025-07-23 09:56:16 +0530

    Fix yazi nvim integration

--- nvim/.config/nvim/lua/plugins/yazi.lua
+++ nvim/.config/nvim/lua/plugins/yazi.lua
@@ -7,7 +7,7 @@ return {
 	keys = {
 		-- 👇 in this section, choose your own keymappings!
 		{
-			"<leader>-chd",
+			"-",
 			mode = { "n", "v" },
 			"<cmd>Yazi<cr>",
 			desc = "Open yazi at the current file",
@@ -35,7 +35,7 @@ return {
 	-- 👇 if you use `open_for_directories=true`, this is recommended
 	init = function()
 		-- More details: https://github.com/mikavilpas/yazi.nvim/issues/802
-		vim.g.loaded_netrw = 1
+		-- vim.g.loaded_netrw = 1
 		vim.g.loaded_netrwPlugin = 1
 	end,
 }
