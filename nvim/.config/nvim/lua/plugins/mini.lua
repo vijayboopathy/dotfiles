121958d159b9cfd8602d7f17388942aab5d42839
vijayboopathy <vijayboopathy.e@gmail.com>
2026-04-22 10:56:17 +0530

    Add support for mini comment

--- nvim/.config/nvim/lua/plugins/mini.lua
+++ nvim/.config/nvim/lua/plugins/mini.lua
@@ -3,6 +3,7 @@ return { -- Collection of various small independent plugins/modules
 	config = function()
 		require("mini.ai").setup({ n_lines = 500 })
 		require("mini.surround").setup()
+		require("mini.comment").setup()
 		local statusline = require("mini.statusline")
 		statusline.setup({ use_icons = vim.g.have_nerd_font })
 		---@diagnostic disable-next-line: duplicate-set-field
